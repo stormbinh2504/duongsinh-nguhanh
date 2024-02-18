8318221bde51267be34b689f4cf3d2ff2292b171
nguyenquan17 <quannguyen2213@gmail.com>
2024-02-18 11:17:05 +0700

    fix conflict

--- src/App.js
+++ src/App.js
@@ -12,42 +12,50 @@ import "../node_modules/slick-carousel/slick/slick.css";
 import "../node_modules/slick-carousel/slick/slick-theme.css";
 
 function App() {
-  const scrollTopAnimated = () => {
-    $('#scrollToTop').on('click', function () {
-      $("html, body").animate({ scrollTop: 0 }, 1200);
-    })
-  }
+  // const scrollTopAnimated = () => {
+  //   $('#scrollToTop').on('click', function () {
+  //     $("html, body").animate({ scrollTop: 0 }, 1200);
+  //   })
+  // }
 
-  let scTimer = 0
   let scrollY = 0
   const handleScroll = () => {
-      if (scTimer) return;
-      scTimer = setTimeout(() => {
-          scrollY = window.scrollY;
-          clearTimeout(scTimer);
-          scTimer = 0;
-      }, 100);
+      const backToTop = document.getElementById('scrollToTop')
+      scrollY = window.scrollY;
+      if(scrollY > 300){
+          backToTop.style.display = 'block'
+      }else{
+          backToTop.style.display = 'none'
+
+      }
+      console.log(scrollY)
   }
+    const toTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    }
 
   useEffect(() => {
-    scrollTopAnimated()
+    // scrollTopAnimated()
   }, []);
 
     useEffect(() => {
+
         window.addEventListener("scroll", handleScroll);
 
-    }, [scrollY]);
+    }, []);
 
   return (
-    <div className="App">
-      {/* <ScrollToTop /> */}
-      <Header />
-      <Home />
-        {scrollY > 300 ?
-            <div id="scrollToTop" className='item-center'><i className="fa fa-angle-double-up" aria-hidden="true"></i>
-            </div> : null}
-        <Footer/>
-    </div>
+      <div className="App">
+          {/* <ScrollToTop /> */}
+          <Header/>
+          <Home/>
+          <div id="scrollToTop" onClick={() => toTop()} className='item-center' ><i className="fa fa-angle-double-up" aria-hidden="true"></i>
+          </div>
+          <Footer/>
+      </div>
   );
 }
 
