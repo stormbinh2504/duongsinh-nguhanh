c0f3f563b834e550c8eeedde12178c3a73c5fb6a
nguyenquan17 <quannguyen2213@gmail.com>
2024-02-22 15:59:03 +0700

    thay banner

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -11,7 +11,7 @@ import HomeLeader from './HomeLeader/HomeLeader';
 import HomeContact from './HomeContact/HomeContact';
 
 import banner_1 from "../../assets/images/home/banner/banner_1.png"
-import banner_mobile from "../../assets/images/home/banner/banner_mobile.png"
+// import banner_mobile from "../../assets/images/home/banner/banner_mobile.png"
 
 const Home = () => {
 
@@ -19,7 +19,7 @@ const Home = () => {
         <div id="container-home" className='container-home'>
             <div id="home-banner" className="home-banner">
                 <img className="banner-desktop" src={banner_1} />
-                <img className="banner-mb" src={banner_mobile} />
+                <img className="banner-mb" src={banner_1} />
             </div>
             <section id="content-home" className="content-home">
                 <div className="content-wrap">
